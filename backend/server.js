d65d41e0851800dffd7c116037e333193badb898
Abel Yohanis <abelyohanis016@gmail.com>
2026-03-08 09:15:34 +0300

    feat: add MongoDB connection setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Mercato API");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
