3a5bebfbd29ece2399e18d0dbaa59c6e53893694
Abel Yohanis <abelyohanis016@gmail.com>
2026-03-08 10:37:49 +0300

    feat: create user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import bcrypt, { genSalt } from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: string,
+        require: true,
+    },
+    email: {
+        type: string,
+        require: true,
+        uniqe: true
+    },
+    password: {
+        type: string,
+        require: true
+    },
+    isAdmin: {
+        type: Boolean,
+        require: true,
+        default: false
+    }
+}, {timestamps: true})
+
+//first let write login functionallty 
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+
+export default mongoose.model("user", userSchema)
