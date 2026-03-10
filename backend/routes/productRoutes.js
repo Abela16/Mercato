866ac4defc7ad89118522a5d29f5ed25f73bf470
Abel Yohanis <abelyohanis016@gmail.com>
2026-03-10 20:04:37 +0300

    feat: implement product search with keyword and pagination

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import createProduct from '../controllers/productControllers.js';
+import {createProduct, getProducts} from '../controllers/productControllers.js';
 import { protect} from '../middleware/authMiddleware.js';
 
 
 const router = express.Router()
 
 router.route('/').post(protect, createProduct)
+router.route('/products').get(getProducts)
 
 export default router;
