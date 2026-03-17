4c3390ac0bb83d3a320ddf78cd207a3167c7e341
Abel Yohanis <abelyohanis016@gmail.com>
2026-03-17 22:35:27 +0300

    feat: add user find by id

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -97,3 +97,13 @@ export const updateUserProfile = asyncHandler( async (req, res) => {
     }
 })
 
+export const getUserById = asyncHandler( async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password');
+
+    if(user){
+        res.json(user)
+    }else{
+        res.status(404).json({message: "user not found"})
+    }
+})
+
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,9 @@ import { registerUser,
          authUser, 
          getProfile, 
          getAllUser, 
-         updateUserProfile} from "../controllers/userControllers.js";
+         updateUserProfile,
+         getUserById,
+        } from "../controllers/userControllers.js";
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router()
@@ -13,23 +15,8 @@ router.post('/login', authUser);
 router.get('/profile', protect, getProfile)
 router.get('/', protect, admin, getAllUser)
 router.post('/:id/update', protect, updateUserProfile)
+router.get('/:id', getUserById)
+
 
 export default router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
