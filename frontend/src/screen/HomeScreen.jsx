1608f9ab93e07d75687f0f8409a5ee922bbc8a03
Abel Yohanis <abelyohanis016@gmail.com>
2026-03-27 23:17:18 +0300

    feat: fetch and display products using redux query

---
+++ frontend/src/screen/HomeScreen.jsx
@@ -0,0 +1,19 @@
+import { useGetProductsQuery } from '../slices/productApiSlice'
+
+function HomeScreen() {
+  const { data, error, isLoading } = useGetProductsQuery()
+
+  if (isLoading) return <h1>Loading...</h1>
+  if (error) return <h1>Error</h1>
+
+  return (
+    <div>
+      <h1>Products</h1>
+      {data.products.map((p) => (
+        <div key={p._id}>{p.name}</div>
+      ))}
+    </div>
+  )
+}
+
+export default HomeScreen
